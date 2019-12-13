0d56993e2f90988d39ea76e11b12f5e23af63dd2
DAM_1 <DAM_1@PB01>
2019-12-13 19:42:33 +0100

    suma

--- src/pruebagib/pruebagib.java
+++ src/pruebagib/pruebagib.java
@@ -1,10 +1,18 @@
 package pruebagib;
 
 public class pruebagib {
+	
+	public static int suma() {
+		int resultado=0;
+		resultado=5+5;
+		return resultado;
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(suma());
 	}
 
 }
