2d7257a533fc805c48de7381a033907cb6cddff0
DAM_1 <DAM_1@PB01>
2019-12-13 20:10:07 +0100

    elevado

--- src/pruebagib/pruebagib.java
+++ src/pruebagib/pruebagib.java
@@ -22,13 +22,18 @@ public class pruebagib {
 		resultado=20/2;
 		return resultado;
 	}
-
+	public static int elevado() {
+		int resultado=0;
+		resultado=(int)Math.pow(5,2);
+		return resultado;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("suma"+suma());
 		System.out.println("resta"+resta());
 		System.out.println("multiplicacion"+multiplicacion());
 		System.out.println("division"+division());
+		System.out.println("elevado"+elevado());
 	}
 
 }
