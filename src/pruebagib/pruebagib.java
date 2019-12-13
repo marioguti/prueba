4d2ba54bb17a64040f38ae2ff3848705cba82576
DAM_1 <DAM_1@PB01>
2019-12-13 19:44:06 +0100

    suma y resta

--- src/pruebagib/pruebagib.java
+++ src/pruebagib/pruebagib.java
@@ -7,12 +7,17 @@ public class pruebagib {
 		resultado=5+5;
 		return resultado;
 	}
-	
+	public static int resta() {
+		int resultado=0;
+		resultado=10-5;
+		return resultado;
+	}
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(suma());
+		System.out.println(resta());
 	}
 
 }
